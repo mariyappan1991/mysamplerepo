2c5d9afc06f32b562b7b4f9828cf6d61dc746eb5
mariyappan1991 <mariyaps1991@gmail.com>
2017-04-01 16:16:32 +0530

    file odeifed

--- databasestest.py
+++ databasestest.py
@@ -3,7 +3,7 @@
 import pymysql
 
 # Open database connection
-db = pymysql.connect("localhost","naukri","root","root" )
+db = pymysql.connect(host="localhost", user="root", passwd="root", db="naukri")
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
