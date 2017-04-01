d68f1cad94bf97c9fa07362335a41a0dd0e6f76c
mariyappan1991 <mariyaps1991@gmail.com>
2017-04-01 08:23:00 +0530

    databasetest.py file is added

---
+++ databasestest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","naukri","root","root" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT * FROM job_applicationform")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print "Application Form : %s " % data
+
+# disconnect from server
+db.close()
