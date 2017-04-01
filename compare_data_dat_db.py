668dac7bfa205aea0face83110de8edc47d6752f
mariyappan1991 <mariyaps1991@gmail.com>
2017-04-01 17:53:07 +0530

    added

--- compare_data_dat_db.py
+++ compare_data_dat_db.py
@@ -16,23 +16,50 @@ all_data = cursor.fetchall()
 
 print ("Application Form : ", all_data)
 
+# get records count
 cursor.execute("SELECT count(*) FROM job_applicationform")
 records_count = cursor.fetchone()
 
 
-# disconnect from server
-db.close()
 
 
+cursor.execute("SELECT * FROM job_applicationform")
+
+
+# Open dat file and read line by line
 with open('naukri_job_applicationform.dat','r') as fo:
-	row = 0
-
+	line_count = 0
+	
 	for line in fo:
-		row += 1
-		print("Row ", row, ": ", line, "\n")
+		line_count += 1
+		print("\n", "Row", line_count, ": ", line)
+		
+		tab_splitted_line = line.strip().split("\t")
+		print(tab_splitted_line)
+		
 
+		# Fetch all row using fetchall() method.
+		db_data = cursor.fetchone()
+		print(db_data,"\n")
+		
+		for i in range(0,len(db_data)-1):
+			# check
+			if tab_splitted_line[i] != str(db_data[i]):
+				print("\n", tab_splitted_line, "is not matched with ", str(db_data), "\n")
+			
+		
+# DB
 print("No of records in DB: ", records_count[0])
 print("DB Record column length: ", len(all_data[0]))
+# DAT File
+print("No of lines in the file: ", line_count)
+print("line\n",len(line.split('\t')))
 
-print("No of lines in the file: ", row)
-print("file line arguments length: ", len(line[0]))
+
+# Value Comparision:
+# compare records count and file line count
+# if count matches, compare row 1, line1, find mismatched records
+# else compare row and line and find mismatched records
+
+# disconnect from server
+db.close()
